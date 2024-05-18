a0197ef23ffe3e2a2ffacd8c6d434b58123c35d0
oCaioOliveira <caiocesar31951910@hotmail.com>
2024-05-18 02:02:16 -0300

    Add docker to app

--- feedfor/feedfor/settings.py
+++ feedfor/feedfor/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = "feedfor.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "feedfordb",
+        "USER": "feedfordbuser",
+        "PASSWORD": "feedfordbpassword",
+        "HOST": "db",
+        "PORT": "5432",
     }
 }
 
