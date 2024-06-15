6c7e5a3e734cc03bfe2ccaa40f5d336a7175020c
ocaioliveira <caio.oliveira@cocobambu.com>
2024-06-15 01:28:46 -0300

    Update serializers fields

--- feedfor/core/serializers.py
+++ feedfor/core/serializers.py
@@ -3,22 +3,32 @@ from .models import Student, Questionnaire, Item
 
 
 class ItemSerializer(serializers.ModelSerializer):
+    questionnaire = serializers.PrimaryKeyRelatedField(queryset=Questionnaire.objects.all())
+
     class Meta:
         model = Item
-        fields = ['question', 'answer', 'subject', 'correct_answer']
+        fields = ['id', 'question', 'subcontent', 'correct_answer', 'created_at', 'questionnaire']
+
 
 
 class QuestionnaireSerializer(serializers.ModelSerializer):
-    items = ItemSerializer(many=True)
+    students = serializers.PrimaryKeyRelatedField(many=True, queryset=Student.objects.all())
 
     class Meta:
         model = Questionnaire
-        fields = ['external_id', 'created_at', 'items']
+        fields = ['id', 'content', 'created_at', 'external_id', 'students']
 
 
 class StudentSerializer(serializers.ModelSerializer):
-    questionnaires = QuestionnaireSerializer(many=True, read_only=True)
-
     class Meta:
         model = Student
-        fields = ['email', 'questionnaires']
+        fields = ['id', 'name', 'email', 'created_at']
+
+
+class AnswerSerializer(serializers.ModelSerializer):
+    students = serializers.PrimaryKeyRelatedField(many=True, queryset=Student.objects.all())
+    item = serializers.PrimaryKeyRelatedField(queryset=Item.objects.all())
+
+    class Meta:
+        model = Answer
+        fields = ['id', 'text', 'feedback', 'created_at', 'students', 'item']
