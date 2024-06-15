1d4877e928ed36d89c1f59a5e5dcc15d7d5f2018
ocaioliveira <caio.oliveira@cocobambu.com>
2024-06-15 03:18:50 -0300

    Refactor view to send gpt task request

--- feedfor/core/views.py
+++ feedfor/core/views.py
@@ -1,32 +1,28 @@
-import json
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from django.core.mail import EmailMessage
-from django.template.loader import render_to_string
-from weasyprint import HTML
-
+from django.conf import settings
 from .models import Student, Questionnaire, Item, Answer
-from .feedback import check_answers, generate_formative_feedback
-from .tasks import send_feedback_email
+from .utils import check_answers
+from .tasks import send_feedback_email, generate_formative_feedback
 
 
 class SubmitQuestionnaireView(APIView):
     def post(self, request):
-        email = request.data.get('email')
-        title = request.data.get('title')
-        content = request.data.get('content')
-        external_id = request.data.get('external_id')
-        items_data = request.data.get('items', [])
+        email = request.data.get("email")
+        title = request.data.get("title")
+        content = request.data.get("content")
+        external_id = request.data.get("external_id")
+        items_data = request.data.get("items", [])
 
         student, _ = Student.objects.get_or_create(email=email)
 
         questionnaire, _ = Questionnaire.objects.get_or_create(
             external_id=external_id,
             defaults={
-                'content': content,
-                'title': title,
-            }
+                "content": content,
+                "title": title,
+            },
         )
         questionnaire.students.add(student)
 
@@ -35,29 +31,38 @@ class SubmitQuestionnaireView(APIView):
         for item_data in items_data:
             item, _ = Item.objects.get_or_create(
                 questionnaire=questionnaire,
-                question=item_data.get('question'),
+                question=item_data.get("question"),
                 defaults={
-                    'subcontent': item_data.get('subcontent'),
-                    'correct_answer': item_data.get('correct_answer')
-                }
+                    "subcontent": item_data.get("subcontent"),
+                    "correct_answer": item_data.get("correct_answer"),
+                },
             )
 
             answer, _ = Answer.objects.get_or_create(
                 item=item,
-                text=item_data.get('answer'),
+                text=item_data.get("answer"),
             )
             answer.students.add(student)
             answers.append(answer)
 
         try:
+            openai_api_key = settings.OPENAI_API_KEY
             feedbacks, correct_count_answers = check_answers(answers)
-            formative_feedback = generate_formative_feedback(feedbacks, content)
+            generate_formative_feedback.delay(
+                feedbacks,
+                content,
+                openai_api_key,
+                email,
+                questionnaire.title,
+                correct_count_answers,
+            )
         except Exception as e:
-            return Response({'error_message': 'Error generating feedback', 'reason': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+            return Response(
+                {"error_message": "Error generating feedback", "reason": str(e)},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
 
-        try:
-            send_feedback_email.delay(email, questionnaire.title, formative_feedback, correct_count_answers)
-        except Exception as e:
-            return Response({'error_message': 'Error sending email', 'reason': str(e)}, status=status.HTTP_400_BAD_REQUEST)
-
-        return Response({'message': 'Questionnaire submitted successfully'}, status=status.HTTP_201_CREATED)
+        return Response(
+            {"message": "Questionnaire submitted successfully"},
+            status=status.HTTP_201_CREATED,
+        )
