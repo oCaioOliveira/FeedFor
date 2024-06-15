694942c1c7f9ac1d081f866987f80ea5432a6cd0
ocaioliveira <caio.oliveira@cocobambu.com>
2024-06-15 01:29:10 -0300

    Add exceptions to endpoint

--- feedfor/core/views.py
+++ feedfor/core/views.py
@@ -6,39 +6,58 @@ from django.core.mail import EmailMessage
 from django.template.loader import render_to_string
 from weasyprint import HTML
 
-from .models import Student, Questionnaire, Item
+from .models import Student, Questionnaire, Item, Answer
 from .feedback import check_answers, generate_formative_feedback
 from .tasks import send_feedback_email
+
 
 class SubmitQuestionnaireView(APIView):
     def post(self, request):
         email = request.data.get('email')
-        name = request.data.get('name')
+        title = request.data.get('title')
         content = request.data.get('content')
         external_id = request.data.get('external_id')
         items_data = request.data.get('items', [])
 
         student, _ = Student.objects.get_or_create(email=email)
 
-        questionnaire = Questionnaire.objects.create(
-            name=name,
-            content=content,
+        questionnaire, _ = Questionnaire.objects.get_or_create(
             external_id=external_id,
-            student=student
+            defaults={
+                'content': content,
+                'title': title,
+            }
         )
+        questionnaire.students.add(student)
+
+        answers = []
 
         for item_data in items_data:
-            Item.objects.create(
+            item, _ = Item.objects.get_or_create(
                 questionnaire=questionnaire,
                 question=item_data.get('question'),
-                answer=item_data.get('answer'),
-                subcontent=item_data.get('subcontent'),
-                correct_answer=item_data.get('correct_answer')
+                defaults={
+                    'subcontent': item_data.get('subcontent'),
+                    'correct_answer': item_data.get('correct_answer')
+                }
             )
 
-        feedbacks, correct_count_answers = check_answers(items_data)
-        formative_feedback = generate_formative_feedback(feedbacks, content)
+            answer, _ = Answer.objects.get_or_create(
+                item=item,
+                text=item_data.get('answer'),
+            )
+            answer.students.add(student)
+            answers.append(answer)
 
-        send_feedback_email.delay(email, questionnaire.name, formative_feedback, correct_count_answers)
+        try:
+            feedbacks, correct_count_answers = check_answers(answers)
+            formative_feedback = generate_formative_feedback(feedbacks, content)
+        except Exception as e:
+            return Response({'error_message': 'Error generating feedback', 'reason': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            send_feedback_email.delay(email, questionnaire.title, formative_feedback, correct_count_answers)
+        except Exception as e:
+            return Response({'error_message': 'Error sending email', 'reason': str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
         return Response({'message': 'Questionnaire submitted successfully'}, status=status.HTTP_201_CREATED)
