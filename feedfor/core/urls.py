87efd154d7e2906da0996ff0318e0f7d75890642
ocaioliveira <caio.oliveira@cocobambu.com>
2024-06-17 02:48:55 -0300

    add url to resend email

--- feedfor/core/urls.py
+++ feedfor/core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import SubmitQuestionnaireView
+from .views import SendFeedbackView, ResendFeedbackView
 
 urlpatterns = [
-    path('submit-questionnaire/', SubmitQuestionnaireView.as_view(), name='submit-questionnaire'),
+    path("send-feedback/", SendFeedbackView.as_view(), name="send-feedback"),
+    path("resend-feedback/", ResendFeedbackView.as_view(), name="resend-feedback"),
 ]
