36f6fea009f7f6c377104b23ce6cdcc34751cd1c
ocaioliveira <caio.oliveira@cocobambu.com>
2024-06-17 05:28:26 -0300

    Create endpoint to send report

--- feedfor/core/urls.py
+++ feedfor/core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import SendFeedbackView, ResendFeedbackView
+from .views import SendFeedbackView, ResendFeedbackView, SendReportView
 
 urlpatterns = [
     path("send-feedback/", SendFeedbackView.as_view(), name="send-feedback"),
     path("resend-feedback/", ResendFeedbackView.as_view(), name="resend-feedback"),
+    path("send-report/", SendReportView.as_view(), name="send-report"),
 ]
