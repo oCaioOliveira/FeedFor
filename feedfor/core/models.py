83b81a235cca1bfb7b8082b7127e2d86133965a4
ocaioliveira <caio.oliveira@cocobambu.com>
2024-06-19 02:51:06 -0300

    Change model settings to assistant settings and made improves

--- feedfor/core/models.py
+++ feedfor/core/models.py
@@ -1,4 +1,8 @@
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
 from django.db import models
+from django.core.exceptions import ValidationError
+from openai import OpenAI
 import uuid
 
 
@@ -22,14 +26,22 @@ class Teacher(models.Model):
         return self.email
 
 
-class ModelSettings(models.Model):
+class AssistantSettings(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     openai_api_key = models.CharField(max_length=255, blank=False, null=False)
+    name = models.CharField(max_length=255, blank=False, null=False)
+    assistant_id = models.CharField(max_length=255, blank=True, null=True)
     model = models.CharField(max_length=255, blank=False, null=False)
-    system_content = models.TextField(blank=False, null=False)
-    max_tokens = models.IntegerField(blank=False, null=False)
+    system_content_instructions = models.TextField(blank=False, null=False)
+    max_completion_tokens = models.IntegerField(blank=False, null=False)
     temperature = models.DecimalField(max_digits=5, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def clean(self):
+        if not (0.009 <= self.temperature <= 2.00):
+            raise ValidationError(
+                {"temperature": "Temperature must be between 0.01 and 2.00."}
+            )
 
 
 class Subject(models.Model):
@@ -38,8 +50,8 @@ class Subject(models.Model):
     code = models.CharField(max_length=255, blank=False, null=False, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
-    model_settings = models.ForeignKey(
-        ModelSettings, related_name="subjects", on_delete=models.RESTRICT
+    assistant_settings = models.ForeignKey(
+        AssistantSettings, related_name="subjects", on_delete=models.RESTRICT
     )
     students = models.ManyToManyField(Student, related_name="subjects")
     teachers = models.ManyToManyField(Teacher, related_name="subjects")
@@ -108,3 +120,41 @@ class Result(models.Model):
 
     def __str__(self):
         return f"{self.id}"
+
+
+@receiver(post_save, sender=AssistantSettings)
+def create_or_update_assistant(sender, instance, created, **kwargs):
+    try:
+        client = OpenAI(api_key=instance.openai_api_key)
+
+        if instance.assistant_id:
+            assistant = client.beta.assistants.update(
+                assistant_id=instance.assistant_id,
+                name=instance.name,
+                instructions=instance.system_content_instructions,
+                temperature=float(instance.temperature),
+                model=instance.model,
+            )
+        else:
+            assistant = client.beta.assistants.create(
+                name=instance.name,
+                instructions=instance.system_content_instructions,
+                temperature=float(instance.temperature),
+                model=instance.model,
+            )
+            instance.assistant_id = assistant.id
+            instance.save()
+    except Exception as e:
+        print(
+            f"Failed to retrieve or create assistant {instance.assistant_id if instance.assistant_id else instance.name}: {str(e)}"
+        )
+
+
+@receiver(post_delete, sender=AssistantSettings)
+def delete_assistant(sender, instance, **kwargs):
+    try:
+        client = OpenAI(api_key=instance.openai_api_key)
+
+        client.beta.assistants.delete(assistant_id=instance.assistant_id)
+    except Exception as e:
+        print(f"Failed to delete assistant {instance.assistant_id}: {str(e)}")
