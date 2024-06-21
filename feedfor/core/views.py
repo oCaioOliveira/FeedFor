3cd516863cf649fb0192cbf1fe003b617f19e8e4
ocaioliveira <caio.oliveira@cocobambu.com>
2024-06-21 01:32:14 -0300

    Remove uselles import

--- feedfor/core/views.py
+++ feedfor/core/views.py
@@ -2,7 +2,6 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
-from django.conf import settings
 from django.core.exceptions import ObjectDoesNotExist
 from .models import (
     Student,
