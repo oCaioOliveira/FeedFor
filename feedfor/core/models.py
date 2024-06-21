8f27a7c65a214161cd435d9a84574e9dba810703
ocaioliveira <caio.oliveira@cocobambu.com>
2024-06-21 02:49:18 -0300

    Add chat settings

--- feedfor/core/models.py
+++ feedfor/core/models.py
@@ -43,6 +43,44 @@ class AssistantSettings(models.Model):
                 {"temperature": "Temperature must be between 0.01 and 2.00."}
             )
 
+    def __str__(self):
+        return self.name
+
+
+class ChatSettings(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    openai_api_key = models.CharField(max_length=255, blank=False, null=False)
+    principal_model = models.CharField(max_length=255, blank=False, null=False)
+    special_model = models.CharField(max_length=255, blank=False, null=False)
+    system_content_instructions = models.TextField(blank=False, null=False)
+    max_tokens = models.IntegerField(blank=False, null=False)
+    temperature = models.DecimalField(max_digits=5, decimal_places=2)
+    top_p = models.DecimalField(max_digits=5, decimal_places=2)
+    frequency_penalty = models.DecimalField(max_digits=5, decimal_places=2)
+    presence_penalty = models.DecimalField(max_digits=5, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def clean(self):
+        if not (0 <= self.temperature <= 2.00):
+            raise ValidationError(
+                {"temperature": "Temperature must be between 0.00 and 2.00."}
+            )
+        if not (0 <= self.top_p <= 1.00):
+            raise ValidationError({"top_p": "Top P must be between 0.00 and 1.00."})
+        if not (0 <= self.frequency_penalty <= 2.00):
+            raise ValidationError(
+                {
+                    "frequency_penalty": "Frequency penalty must be between 0.00 and 2.00."
+                }
+            )
+        if not (0 <= self.presence_penalty <= 2.00):
+            raise ValidationError(
+                {"presence_penalty": "Presence penalty must be between 0.00 and 2.00."}
+            )
+
+    def __str__(self):
+        return f"{self.id}"
+
 
 class Subject(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
@@ -50,8 +88,8 @@ class Subject(models.Model):
     code = models.CharField(max_length=255, blank=False, null=False, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
-    assistant_settings = models.ForeignKey(
-        AssistantSettings, related_name="subjects", on_delete=models.RESTRICT
+    chat_settings = models.ForeignKey(
+        ChatSettings, related_name="subjects", on_delete=models.RESTRICT
     )
     students = models.ManyToManyField(Student, related_name="subjects")
     teachers = models.ManyToManyField(Teacher, related_name="subjects")
@@ -73,7 +111,7 @@ class Questionnaire(models.Model):
     students = models.ManyToManyField(Student, related_name="questionnaires")
 
     def __str__(self):
-        return f"{self.id}"
+        return f"{self.title} - {self.external_id}"
 
 
 class Item(models.Model):
@@ -88,7 +126,7 @@ class Item(models.Model):
     )
 
     def __str__(self):
-        return f"{self.id}"
+        return f"{self.question[:20]}..."
 
 
 class Answer(models.Model):
@@ -103,7 +141,7 @@ class Answer(models.Model):
     item = models.ForeignKey(Item, related_name="answers", on_delete=models.RESTRICT)
 
     def __str__(self):
-        return f"{self.id}"
+        return f"{self.text[:20]}..."
 
 
 class Result(models.Model):
@@ -119,7 +157,7 @@ class Result(models.Model):
     )
 
     def __str__(self):
-        return f"{self.id}"
+        return f"{self.score}"
 
 
 @receiver(post_save, sender=AssistantSettings)
